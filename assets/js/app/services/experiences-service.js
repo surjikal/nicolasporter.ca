f49baa04c76f1a26622271c3fe5d48e27ab5fe22
Nicolas Porter <porter.nicolas@gmail.com>
2013-03-20 21:41:16 -0400

    Updated.

--- assets/js/app/services/experiences-service.js
+++ assets/js/app/services/experiences-service.js
@@ -3,6 +3,19 @@
   app.service('experiences', function() {
     return [
       {
+        location: '42',
+        position: 'Lead Software Developer',
+        terms: [
+          {
+            duration: {
+              start: 'February 2013',
+              stop: 'Current'
+            },
+            tasks: ["Built hybrid mobile web app using AngularJS and Cordova/PhoneGap.", "Built an analytics dashboard (AngularJS), featuring a cache-manifest-based offline mode.", "Finalist at the first Fashion Hackathon in NYC, presented the product on the runway during Fashion Week NYC."],
+            technologies: ["JavaScript", "AngularJS", "Node.js, Express", "Git", "jQuery, Zepto", "Fabric", "HTML, Jade", "CSS, Sass, Compass", "Nginx", "GNU/Linux", "Bash, Zsh"]
+          }
+        ]
+      }, {
         location: 'Trolley',
         position: 'Lead Software Developer',
         terms: [
@@ -16,19 +29,6 @@
           }
         ]
       }, {
-        location: '42',
-        position: 'Mobile Web Developer',
-        terms: [
-          {
-            duration: {
-              start: 'February 2013',
-              stop: 'Current'
-            },
-            tasks: ["Built hybrid mobile web app using AngularJS and Cordova/PhoneGap", "Built analytics dashboard (AngularJS), featuring a cache-manifest-based offline mode (for demo purposes)", "Finalist at the first Fashion Hackathon in NYC (team 42)"],
-            technologies: ["JavaScript", "AngularJS", "Node.js, Express", "Git", "jQuery, Zepto", "Fabric", "HTML, Jade", "CSS, Sass, Compass", "Nginx", "GNU/Linux", "Bash, Zsh"]
-          }
-        ]
-      }, {
         location: 'Benbria',
         position: 'Software Developer',
         terms: [
@@ -37,21 +37,21 @@
               start: 'April 2012',
               stop: 'September 2012'
             },
-            tasks: ["Node.js development on a multi-tenant web app (BlazeLoop)", "Designed and implemented rule-based, dynamic API system, significantly improving mantainability and extensibility of app", "Installed metrics stack (StatsD & Graphite), integrated it with app"],
+            tasks: ["Node.js development on a multi-tenant web app (BlazeLoop)", "Designed and implemented a dynamic API system, significantly improving maintainability of BlazeLoop", "Conducted multiple in-person technical interviews"],
             technologies: ["JavaScript", "Git", "jQuery, Zepto", "HTML, Jade", "CSS, Sass, Compass", "Nginx", "GNU/Linux", "Bash, Zsh", "Node.js, Express", "MongoDB"]
           }, {
             duration: {
               start: 'April 2011',
               stop: 'September 2011'
             },
-            tasks: ["Designed and built prototype for the BlazeLoop web app using Django and Sencha Touch"],
+            tasks: ["Designed and built prototype for a new mobile web app, BlazeLoop", "Fixed BlazeCast's (Benbria's main product)"],
             technologies: ["JavaScript", "jQuery, Zepto", "Git", "HTML, Jade", "CSS, Sass, Compass", "GNU/Linux", "Bash, Zsh"]
           }, {
             duration: {
               start: 'April 2010',
               stop: 'September 2010'
             },
-            tasks: ["Python and Django development on web app for one of the largest restaurant chains in the US"],
+            tasks: ["Built and shipped a Django-based web app for one of the largest restaurant chains in the US"],
             technologies: ["Git", "Python, Django", "GNU/Linux", "Bash, Zsh"]
           }
         ]
