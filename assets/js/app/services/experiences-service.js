ef1fd26052d128f9d71bb95a9e9d426feca4fcbc
Nicolas Porter <porter.nicolas@gmail.com>
2013-03-20 22:30:57 -0400

    Updated.

--- assets/js/app/services/experiences-service.js
+++ assets/js/app/services/experiences-service.js
@@ -11,7 +11,7 @@
               start: 'February 2013',
               stop: 'Current'
             },
-            tasks: ["Built hybrid mobile web app using AngularJS and Cordova/PhoneGap.", "Built an analytics dashboard (AngularJS), featuring a cache-manifest-based offline mode.", "Finalist at the first Fashion Hackathon in NYC, presented the product on the runway during Fashion Week NYC."],
+            tasks: ["Crafted a framework for rapid prototyping of mobile web apps with a native-like user experience", "Built an analytics dashboard featuring a cache-manifest-based offline mode", "Designed a distributed architecture where many specialized processes act as independent web services", "Finalist at the first Fashion Hackathon in NYC, presented the product on the runway during NYC Fashion Week"],
             technologies: ["JavaScript", "AngularJS", "Node.js, Express", "Git", "jQuery, Zepto", "Fabric", "HTML, Jade", "CSS, Sass, Compass", "Nginx", "GNU/Linux", "Bash, Zsh"]
           }
         ]
@@ -24,7 +24,7 @@
               start: 'April 2012',
               stop: 'Current'
             },
-            tasks: ["Built hybrid mobile web app for m-commerce, frontend using Backbone and Cordova, backend is REST-style API (Django)", "Developed highly customized GruntJS-based toolchain, reducing setup times from a day to seconds", "Finalists at the Ottawa Startup Weekend (team Virtual Plaza)"],
+            tasks: ["Built a hybrid mobile web app for an m-commerce venture; integrated with Stripe and PayPal APIs", "Developed a highly customized GruntJS-based toolchain, reducing setup times from a day to seconds", "Finalists at the Ottawa Startup Weekend (team Virtual Plaza)"],
             technologies: ["JavaScript", "Git", "Backbone", "jQuery, Zepto", "Python, Django", "Fabric", "HTML, Jade", "CSS, Sass, Compass", "Nginx", "GNU/Linux", "Bash, Zsh"]
           }
         ]
@@ -37,14 +37,14 @@
               start: 'April 2012',
               stop: 'September 2012'
             },
-            tasks: ["Node.js development on a multi-tenant web app (BlazeLoop)", "Designed and implemented a dynamic API system, significantly improving maintainability of BlazeLoop", "Conducted multiple in-person technical interviews"],
+            tasks: ["Node.js development on a multi-tenant web app (BlazeLoop)", "Designed and implemented a dynamic API system, significantly improving BlazeLoop's maintainability", "Conducted multiple in-person technical interviews"],
             technologies: ["JavaScript", "Git", "jQuery, Zepto", "HTML, Jade", "CSS, Sass, Compass", "Nginx", "GNU/Linux", "Bash, Zsh", "Node.js, Express", "MongoDB"]
           }, {
             duration: {
               start: 'April 2011',
               stop: 'September 2011'
             },
-            tasks: ["Designed and built prototype for a new mobile web app, BlazeLoop", "Fixed BlazeCast's (Benbria's main product)"],
+            tasks: ["Designed and built prototype for a new mobile web app, BlazeLoop"],
             technologies: ["JavaScript", "jQuery, Zepto", "Git", "HTML, Jade", "CSS, Sass, Compass", "GNU/Linux", "Bash, Zsh"]
           }, {
             duration: {
