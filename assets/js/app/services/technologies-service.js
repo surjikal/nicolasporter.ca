7f173f775e61a8b42d11c838b1bf4a5ab2759cb0
Nick Porter <porter.nicolas@gmail.com>
2013-11-23 17:48:37 -0500

    Update technologies-service.js

--- assets/js/app/services/technologies-service.js
+++ assets/js/app/services/technologies-service.js
@@ -1,7 +1,7 @@
 (function() {
 
   app.service('technologies', function() {
-    return [["JavaScript", "AngularJS", "Backbone", "jQuery, Zepto"], ["Node.js, Express", "MongoDB", "HTML, Jade", "CSS, Sass, Compass"], ["Python, Django", "Fabric", "Nginx", "Git"], ["GNU/Linux", "Bash, Zsh", "Java", "C++, QT, OpenGL"]];
+    return [["JavaScript", "AngularJS", "Backbone", "jQuery, Zepto"], ["Node.js, Express", "MongoDB", "HTML, Jade", "CSS, Sass, Compass"], ["Python, Django", "Docker", "Nginx", "Git"], ["GNU/Linux", "Bash, Zsh", "Java", "C++, QT, OpenGL"]];
   });
 
 }).call(this);
